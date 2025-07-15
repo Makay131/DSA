22d0699484ed41bed501b7dc709dd2d9d14931e3
Makay131 <mahmutenesakay9@gmail.com>
2025-07-15 14:49:20 +0300

    first commit

---
+++ main/bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort (arr, comparator = (a,b) => a - b) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for(const i = 0; i < arr.length; i++) {
+      if(comparator(arr[i], arr[i+1]) > 0) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped)
+
+  return arr;
+}
